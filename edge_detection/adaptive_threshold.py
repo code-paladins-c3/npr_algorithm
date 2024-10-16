0e11b412e2b4ffee409b80e4f5ae8a12d8d9784c
igor-c-assuncao <igor.00000829605@unicap.br>
2024-10-16 11:29:44 -0300

    calculo do Gradient Resolution

--- edge_detection/adaptive_threshold.py
+++ edge_detection/adaptive_threshold.py
@@ -1,22 +1,35 @@
 import numpy as np
 
 def calculate_threshold(gradient_magnitude, tolerance=1e-3, alpha=0.7):
+    
     T0 = np.max(gradient_magnitude)
     T1 = np.min(gradient_magnitude)
-    adapative_threshold = (T0 + T1) / 2
+    
+   
+    T_squared = (T0 * alpha) ** 2
+
+    
+    adapative_threshold = (T_squared + T1) / 2
 
     while True:
+        
         Ta_values = gradient_magnitude[gradient_magnitude >= adapative_threshold]
         Tb_values = gradient_magnitude[gradient_magnitude < adapative_threshold]
-        
+
+    
         Ta = np.mean(Ta_values) if len(Ta_values) > 0 else 0
         Tb = np.mean(Tb_values) if len(Tb_values) > 0 else 0
 
+        
         new_T = (Ta + Tb) / 2
 
+       
         if abs(new_T - adapative_threshold) < tolerance:
             break
 
         adapative_threshold = new_T
-        
+
+    
+    gradient_magnitude[gradient_magnitude < adapative_threshold] = 0
+
     return adapative_threshold
