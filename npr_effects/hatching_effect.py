988f5d79db980fd73b845ef98f7e248451d73320
Victor Macaubas <victor@macaubas.com>
2024-11-11 13:47:14 -0300

    update apply hatching

--- npr_effects/hatching_effect.py
+++ npr_effects/hatching_effect.py
@@ -5,25 +5,22 @@ from edge_stylization.hatching import apply_hatching
 from edge_stylization.edge_coloring import color_edges
 from edge_stylization.line_width import adjust_line_width
 
-def apply_hatching_effect(image, patterns_dir='texture/hatching_patterns', num_levels=5):
-    """
-    Applies a hatching effect to the image.
-
-    Parameters:
-    - image: Original BGR image.
-    - patterns_dir: Directory containing hatching patterns.
-    - num_levels: Number of intensity levels.
-
-    Returns:
-    - hatched_image: Image with hatching effect applied.
-    """
+def apply_hatching_effect(image, patterns_dir='texture/hatching_patterns', num_levels=5, use_gradient=False):
     gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
     edges = modified_canny_detector(image)
     thick_edges = adjust_line_width(edges, min_thickness=1, max_thickness=2)
     colored_edges = color_edges(thick_edges, color=(0, 0, 0))
 
-    hatched = apply_hatching(image, gray_image, patterns_dir, num_levels=num_levels)
+    if use_gradient:
+        grad_x = cv.Sobel(gray_image, cv.CV_64F, 1, 0, ksize=3)
+        grad_y = cv.Sobel(gray_image, cv.CV_64F, 0, 1, ksize=3)
+        gradient_magnitude = cv.magnitude(grad_x, grad_y)
+        intensity_map = gradient_magnitude
+    else:
+        intensity_map = gray_image
+
+    hatched = apply_hatching(image, intensity_map, patterns_dir, num_levels=num_levels)
 
     hatched_with_edges = cv.addWeighted(hatched, 1, colored_edges, 1, 0)
 
