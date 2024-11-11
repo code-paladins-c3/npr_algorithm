0834d6fb56a7e195c7e589e16fd43510a74c46a3
Victor Macaubas <victor@macaubas.com>
2024-11-11 15:14:16 -0300

    final changes

--- example/run_npr_effects.py
+++ example/run_npr_effects.py
@@ -8,16 +8,16 @@ from utils.view import display_image
 from npr_effects.cartoon import apply_cartoon_effect
 from npr_effects.pencil import apply_pencil_sketch, apply_pencil_sketch_with_texture
 
-image = read_image('input/knight.jpeg')
+image = read_image('input/hercules.jpeg')
 
 cartoon_image = apply_cartoon_effect(image, num_posterization_levels=4)
-save_image('output/cartoon_effect.jpg', cartoon_image)
-display_image('Cartoon Effect', cartoon_image, 1000)
+save_image('output/cartoon_effect_hercules.jpg', cartoon_image)
+display_image('Cartoon Effect', cartoon_image, 10000)
 
 sketch_image = apply_pencil_sketch(image, blend_factor=0.5)
-save_image('output/pencil_sketch.jpg', sketch_image)
-display_image('Pencil Sketch', sketch_image, 1000)
+save_image('output/pencil_sketch_hercules.jpg', sketch_image)
+display_image('Pencil Sketch', sketch_image, 10000)
 
 sketch_texture_image = apply_pencil_sketch_with_texture(image, 'texture/paper_texture.jpg', blend_factor=0.5)
-save_image('output/pencil_sketch_with_texture.jpg', sketch_texture_image)
-display_image('Pencil Sketch with Texture', sketch_texture_image, 1000)
+save_image('output/pencil_sketch_with_texture_hercules.jpg', sketch_texture_image)
+display_image('Pencil Sketch with Texture', sketch_texture_image, 10000)
