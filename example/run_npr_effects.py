9582f55ddb7913fda3ad5ddb3a6bd064bff760fc
Victor Macaubas <victor@macaubas.com>
2024-11-11 15:54:35 -0300

    improvents

--- example/run_npr_effects.py
+++ example/run_npr_effects.py
@@ -8,16 +8,28 @@ from utils.view import display_image
 from npr_effects.cartoon import apply_cartoon_effect
 from npr_effects.pencil import apply_pencil_sketch, apply_pencil_sketch_with_texture
 
-image = read_image('input/hercules.jpeg')
+import os
 
-cartoon_image = apply_cartoon_effect(image, num_posterization_levels=4)
-save_image('output/cartoon_effect_hercules.jpg', cartoon_image)
-display_image('Cartoon Effect', cartoon_image, 10000)
+input_folder = 'input'
+output_folder = 'output'
+texture_path = 'texture/paper_texture.jpg'
 
-sketch_image = apply_pencil_sketch(image, blend_factor=0.5)
-save_image('output/pencil_sketch_hercules.jpg', sketch_image)
-display_image('Pencil Sketch', sketch_image, 10000)
+for filename in os.listdir(input_folder):
+    if filename.endswith('.jpeg') or filename.endswith('.jpg') or filename.endswith('.png'):
+        image_path = os.path.join(input_folder, filename)
+        image = read_image(image_path)
 
-sketch_texture_image = apply_pencil_sketch_with_texture(image, 'texture/paper_texture.jpg', blend_factor=0.5)
-save_image('output/pencil_sketch_with_texture_hercules.jpg', sketch_texture_image)
-display_image('Pencil Sketch with Texture', sketch_texture_image, 10000)
+        cartoon_image = apply_cartoon_effect(image, num_posterization_levels=4)
+        cartoon_output_path = os.path.join(output_folder, f'cartoon_effect_{filename}')
+        save_image(cartoon_output_path, cartoon_image)
+        display_image('Cartoon Effect', cartoon_image, 10000)
+
+        sketch_image = apply_pencil_sketch(image, blend_factor=0.5)
+        sketch_output_path = os.path.join(output_folder, f'pencil_sketch_{filename}')
+        save_image(sketch_output_path, sketch_image)
+        display_image('Pencil Sketch', sketch_image, 10000)
+
+        sketch_texture_image = apply_pencil_sketch_with_texture(image, texture_path, blend_factor=0.5)
+        sketch_texture_output_path = os.path.join(output_folder, f'pencil_sketch_with_texture_{filename}')
+        save_image(sketch_texture_output_path, sketch_texture_image)
+        display_image('Pencil Sketch with Texture', sketch_texture_image, 10000)
